b80066a94c456e7e201addf38eaa619d1706760a
Gopikalakshmi <gopikalakshmia@gmail.com>
2025-03-13 00:26:42 -0400

    edit,delete and toasted added

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -27,17 +27,71 @@ public class CategoryController : Controller
     public IActionResult Create(){
         return View();
     }
-//Form submission
+//Create Form submission
     [HttpPost]
     public IActionResult Create(Category obj){
         if(ModelState.IsValid){
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"]="The category Added Successfully";
             return RedirectToAction("Index");
         }
         else
         return View();
     }
+//Edit
+public IActionResult Edit (int? Id){
+    if(Id==null && Id==0){
+        return NotFound();
+    }
+    Category? editCategoryVal = _db.Categories.FirstOrDefault(item => item.Id == Id);
+    if (editCategoryVal == null)
+    {
+        return NotFound();
+    }
+    return View(editCategoryVal);
+}
+//Submit Edit
+    [HttpPost]
+    public IActionResult Edit(Category obj){
+        if(ModelState.IsValid){
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+               TempData["success"]="The category Edited Successfully";
+            return RedirectToAction("Index");
+        }
+        else
+        return View();
+    }
+
+//Delete
+public IActionResult Delete(int? Id){
+        if (Id == null && Id == 0)
+        {
+        return NotFound();
+    }
+    Category? deleteCategoryVal = _db.Categories.FirstOrDefault(item => item.Id == Id);
+    if (deleteCategoryVal == null)
+    {
+        return NotFound();
+    }
+    return View(deleteCategoryVal);
+}
+//Submit Delete
+    [HttpPost,ActionName("Delete")]
+    public IActionResult DeletePost(Category obj){
+     
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+               TempData["success"]="The category Removed Successfully";
+            return RedirectToAction("Index");
+       
+        
+    }
+
+
+
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
--- obj/Debug/net8.0/onlineBookstore.AssemblyInfo.cs
+++ obj/Debug/net8.0/onlineBookstore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("onlineBookstore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae24c10935e6431b868811f1bc0a6fe1ccb3624")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117ef907f40ef63cb11a2dc001a60b109dcd3bc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("onlineBookstore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("onlineBookstore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
